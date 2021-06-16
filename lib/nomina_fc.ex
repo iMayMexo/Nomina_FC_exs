f5c92ea45b670cccff5ae28df8b56cfa5f9e97b0
Carlos <desarrollo@america-carrental.com>
2021-06-16 15:28:29 -0500

    Main

---
+++ lib/nomina_fc.ex
@@ -0,0 +1,18 @@
+defmodule NominaFc do
+  @moduledoc """
+  Documentation for `NominaFc`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NominaFc.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nomina_fc_test.exs
@@ -0,0 +1,8 @@
+defmodule NominaFcTest do
+  use ExUnit.Case
+  doctest NominaFc
+
+  test "greets the world" do
+    assert NominaFc.hello() == :world
+  end
+end
