b40c5f289594ec0df08baa7b8317efc58ef5e5e3
Carlos <desarrollo@america-carrental.com>
2021-06-18 01:45:30 -0500

    Update import.ex
    Import json data

--- lib/import.ex
+++ lib/import.ex
@@ -1,8 +1,29 @@
 defmodule NominaFc.Import do
+  alias NominaFc.Players, as: Players
 
   def import() do
-    Jason.decode(File.read!("assets/data.json"))
-    {:ok, Enum.into([{"result", "this will be an Elixir result"}], Map.new)}
+    {:ok, data} = Jason.decode(File.read!("assets/data.json"))
+    data["jugadores"]
   end
+
+  defp to_map(%Players{
+    nombre:           n,
+    nivel:            l,
+    goles:            g,
+    sueldo:           s,
+    bono:             b,
+    sueldo_completo:  sc,
+    equipo:           t
+    }) do
+      %{
+        "nombre"          => n,
+        "nivel"           => l,
+        "goles"           => g,
+        "sueldo"          => s,
+        "bono"            => b,
+        "sueldo_completo" => sc,
+        "equipo"          => t
+      }
+    end
 
 end
